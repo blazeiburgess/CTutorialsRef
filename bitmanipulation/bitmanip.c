dfafdb24ac5e0f655286078354c7504509a62977
blazeiburgess <blaze.i.burgess@gmail.com>
2016-10-07 15:43:06 -0400

    Add sign function

--- bitmanipulation/bitmanip.c
+++ bitmanipulation/bitmanip.c
@@ -1,3 +1,6 @@
+// Originally from:
+// https://github.com/leagerl1/BitManipulation/blob/master/bits.c
+//
 // checks if integer is zero by inversing zero (false) or one (true) 
 // to return the opposite
 int isZero(int x) {
@@ -13,4 +16,13 @@ int evenOddSwap(int x) {
   int evens = x & evenMask;
   int odds = x & oddMask;
   return ((evens >> 1)&(~(1<<31)))|(odds << 1);
+}
+
+// returns first mask (1 for positive value, 0 for zero) or second mask
+// (0 for 0 or positive numbers, -1 for negative number)
+int sign(int x) {
+  int mask = ~(1 << 31);
+  int posOrOther = ~~(x & mask);
+  int mask2 = x >> 31;
+  return (posOrOther | mask2);
 }
