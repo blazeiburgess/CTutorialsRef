83c220d13baec9b2dd6e4778ef62233ee19cdf69
blazeiburgess <blaze.i.burgess@gmail.com>
2016-10-07 15:37:08 -0400

    Add function to swap even and odd bits

--- bitmanipulation/bitmanip.c
+++ bitmanipulation/bitmanip.c
@@ -4,4 +4,13 @@ int isZero(int x) {
   return !x;
 }
 
-
+// creates mask for even bits, mask for odd bits (flipped even bits),
+// then pulls those bits into respective `even` and `odd` variables
+// by the `x & <mask>` syntax
+int evenOddSwap(int x) {
+  int evenMask = (0xAA << 24)|(0xAA << 16)|(0xAA << 8)|(0xAA);
+  int oddMask = ~evenMask;
+  int evens = x & evenMask;
+  int odds = x & oddMask;
+  return ((evens >> 1)&(~(1<<31)))|(odds << 1);
+}
